888c68483a0762db557278a64efc8c8e1503cd85
Shinmera <shinmera@tymoon.eu>
2017-03-16 17:25:49 +0100

    Don't print a semicolon after function definitions.

--- printer.lisp
+++ printer.lisp
@@ -316,4 +316,5 @@
           (T
            (indent)
            (serialize-part item)
-           (sformat ";")))))
+           (unless (eql 'function-definition (first item))
+             (sformat ";"))))))
